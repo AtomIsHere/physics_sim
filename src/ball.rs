33f26eda08f200f98d3794d71de2f8c5a8d08724
Archie O'Connor <archietomoconnor@gmail.com>
2022-12-06 20:07:36 +1100

    Add gravity

--- src/ball.rs
+++ src/ball.rs
@@ -1,5 +1,7 @@
 use bevy::prelude::{Component, Query, Res, Transform, Windows};
 
+pub const GRAVITY: f32 = 0.1;
+
 #[derive(Component)]
 pub struct Ball {
     pub mass: f32,
@@ -9,10 +11,21 @@ pub struct Ball {
 #[derive(Component)]
 pub struct BallMovement {
     pub direction: f32,
-    pub speed: f32
+    pub velocity: f32
 }
 
-pub fn ball_movement_system(windows: Res<Windows>, mut query: Query<(&Ball, &BallMovement, &mut Transform)>) {
-    let (ball, ball_movement, mut transform) = query.single_mut();
-    let window = windows.get_primary().unwrap();
+pub fn ball_movement_system(mut query: Query<(&Ball, &mut BallMovement, &mut Transform)>) {
+    // Acquire data from query
+    let (_, mut ball_movement, mut transform) = query.single_mut();
+
+    // Calculate velocity
+    let dx = ball_movement.velocity * ball_movement.direction.cos();
+    let dy = ball_movement.velocity * ball_movement.direction.sin();
+
+    // Apply velocity
+    transform.translation.x += dx;
+    transform.translation.y += dy;
+
+    ball_movement.velocity = (dx.powi(2) + (dy - GRAVITY).powi(2)).sqrt();
+    ball_movement.direction = ((dy - GRAVITY)/dx).atan();
 }
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,39 @@
 mod ball;
 
 use bevy::prelude::*;
+use bevy::sprite::MaterialMesh2dBundle;
+use bevy::time::FixedTimestep;
+use crate::ball::{Ball, ball_movement_system, BallMovement};
+
+pub const BALL_RADIUS: f32 = 10.;
+
+pub const FPS: i64 = 60;
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
+        .add_startup_system(setup)
+        .add_system_set(
+            SystemSet::new()
+                .with_run_criteria(FixedTimestep::step(1.0 / (FPS as f64)))
+                .with_system(ball_movement_system)
+        )
         .run();
-
-
 }
 
-#[derive(Component)]
-pub struct Constants {
-    pub gravity: f32
+fn setup(mut commands: Commands, mut meshes: ResMut<Assets<Mesh>>, mut materials: ResMut<Assets<ColorMaterial>>) {
+    commands.spawn(Camera2dBundle::default());
+
+    commands.spawn(MaterialMesh2dBundle {
+        mesh: meshes.add(shape::Circle::new(BALL_RADIUS).into()).into(),
+        material: materials.add(ColorMaterial::from(Color::WHITE)),
+        transform: Transform::from_translation(Vec3::new(0., 0., 0.)),
+        ..default()
+    }).insert(Ball {
+        mass: 1.,
+        elasticity: 0.8,
+    }).insert(BallMovement {
+        direction: 1.,
+        velocity: 1.,
+    });
 }
