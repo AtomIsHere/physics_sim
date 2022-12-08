9f5f28b7f21364070c32633900b82b5297b92a4d
AtomIsHere <archietomoconnor@gmail.com>
2022-12-08 12:01:22 +1100

    Implement arrows with bevy lyon

---
+++ src/arrow.rs
@@ -0,0 +1,34 @@
+use bevy_prototype_lyon::geometry::Geometry;
+use bevy_prototype_lyon::prelude::tess::geom::euclid::{Box2D, Size2D};
+use bevy_prototype_lyon::prelude::tess::geom::{Point, point};
+use bevy_prototype_lyon::prelude::tess::path::path::Builder;
+use bevy_prototype_lyon::prelude::tess::path::{Polygon, Winding};
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct Arrow {
+    pub rectangle_length: f32,
+    pub rectangle_width: f32,
+    pub triangle_offset: f32,
+    pub triangle_height: f32,
+}
+
+impl Geometry for Arrow {
+    fn add_geometry(&self, b: &mut Builder) {
+        let rectangle_origin = Point::new(0., 0.);
+
+        b.add_rectangle(
+            &Box2D::from_origin_and_size(rectangle_origin, Size2D::new(self.rectangle_width, self.rectangle_length)),
+            Winding::Positive
+        );
+        b.add_polygon(
+            Polygon {
+                points: &[
+                    point(-self.triangle_offset, self.rectangle_length),
+                    point(self.triangle_offset + self.rectangle_width, self.rectangle_length),
+                    point(self.rectangle_width/2., self.rectangle_length + self.triangle_height)
+                ],
+                closed: true
+            }
+        );
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
 mod ball;
+mod arrow;
 
 use bevy::prelude::*;
 use bevy::sprite::MaterialMesh2dBundle;
 use bevy::time::FixedTimestep;
+use bevy_prototype_lyon::draw::{DrawMode, FillMode};
+use bevy_prototype_lyon::prelude::{GeometryBuilder, ShapePlugin};
+use crate::arrow::Arrow;
 use crate::ball::{Ball, ball_bounce_system, ball_movement_system, BallMovement};
 
 pub const BALL_RADIUS: f32 = 10.;
@@ -12,6 +16,7 @@ pub const FPS: i64 = 60;
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
+        .add_plugin(ShapePlugin)
         .add_startup_system(setup)
         .add_system_set(
             SystemSet::new()
@@ -23,6 +28,12 @@ fn main() {
 }
 
 fn setup(mut commands: Commands, mut meshes: ResMut<Assets<Mesh>>, mut materials: ResMut<Assets<ColorMaterial>>) {
+    let arrow = Arrow {
+        rectangle_length: BALL_RADIUS + 10.,
+        rectangle_width: 5.,
+        triangle_offset: 3.,
+        triangle_height: 10.,
+    };
     commands.spawn(Camera2dBundle::default());
 
     commands.spawn(MaterialMesh2dBundle {
@@ -37,4 +48,10 @@ fn setup(mut commands: Commands, mut meshes: ResMut<Assets<Mesh>>, mut materials
         dx: 1.,
         dy: 1.,
     });
+
+    commands.spawn(GeometryBuilder::build_as(
+        &arrow,
+        DrawMode::Fill(FillMode::color(Color::WHITE)),
+        Transform::default()
+    ));
 }
